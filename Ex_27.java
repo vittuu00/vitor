632f514a912ad6fc8e41a5bab1d879943b620a91
vittuu00 <53568844+vittuu00@users.noreply.github.com>
2019-08-07 21:50:56 -0300

    Add files via upload

--- Ex_27.java
+++ Ex_27.java
@@ -1,4 +1,49 @@
+import java.util.Scanner;
 
 public class Ex_27 {
 
-}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float result = 0;
+        System.out.println("Digite um número: ");
+        float num = scan.nextInt();
+
+        System.out.println("Digite mais um numero: ");
+        float num2 = scan.nextInt();
+
+        System.out.println("Agora, que operação deseja fazer: (1: +)(2: -)(3: *)(4: /)");
+        int op = scan.nextInt();
+        switch(op) {
+            case 1:
+                result = num + num2;
+            break;
+            case 2:
+                result = num - num2;
+            break;
+            case 3:
+                result = num * num2;
+            break;
+            case 4:
+                result = num / num2;
+            break;
+        }
+        System.out.println(result);
+
+        if (result % 2 == 0) {
+            System.out.println("O resultado é par");
+        }
+        else {
+            System.out.println("O resultado é impar");
+        }
+        if(result >= 0) {
+            System.out.println("positivo");
+        }
+        else {
+            System.out.println("negativo");
+        }
+        if(result % 1 == 0 ) {
+            System.out.println("inteiro");
+        }
+        else {
+            System.out.println("decimal");
+        }
--- Ex_28.java
+++ Ex_28.java
@@ -1,4 +1,58 @@
+import java.util.Scanner;
 
 public class Ex_28 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int resp = 0;
+        System.out.println("Você está sendo interrogado!\nResponda as perguntas com apenas True e False (Diga somente a verdade)");
+
+        System.out.println("Telefonou para a vítima?");
+        boolean resp1 = entrada.nextBoolean();
+
+        if (resp1 == true) { 
+            resp = resp + 1;
+        }
+        System.out.println("Esteve no local do crime?");
+        boolean resp2 = entrada.nextBoolean();
+
+        if (resp2 == true) {
+            resp = resp + 1;
+        }
+
+        System.out.println("Mora perto da vítima?");
+        boolean resp3 = entrada.nextBoolean();
+
+        if (resp3 == true) {
+            resp = resp + 1;
+        }
+
+        System.out.println("Devia para a vítima?");
+        boolean resp4 = entrada.nextBoolean();
+
+        if (resp4 == true) {
+            resp = resp + 1;
+        }
+
+        System.out.println("Já trabalhou com a vítima?");
+        boolean resp5 = entrada.nextBoolean();
+
+        if (resp5 == true) {
+            resp = resp + 1;
+        }
+
+        System.out.println();
+        if (resp == 2) {
+            System.out.println("Suspeita");
+        }else if ((resp == 3) | (resp == 4)) {
+            System.out.println("Cúmplice");
+        }else if (resp == 5) {
+            System.out.println("Assassino");
+        }else 
+            System.out.println("Inocente");
+
+    }
 
 }
